3233dcad309af000cfabdd22cd626f9f328eb563
David Kahle <david.kahle@gmail.com>
2015-03-05 07:22:08 +0000

    version 2.4

--- R/mapdist.R
+++ R/mapdist.R
@@ -1,7 +1,7 @@
 #' Compute map distances using Google
 #'
 #' Compute map distances using Google Maps. Note that in most cases by using this function you are agreeing to the Google Maps API Terms of Service at https://developers.google.com/maps/terms.
-#' 
+#'
 #' @param from name of origin addresses in a data frame (vector accepted)
 #' @param to name of destination addresses in a data frame (vector accepted)
 #' @param output amount of output
@@ -17,9 +17,8 @@
 #' @export
 #' @examples
 #'
-#' 
-#' \dontrun{
-#' 
+#' mapdist("waco, texas", "houston, texas")
+#'
 #' from <- c('houston, texas', 'dallas')
 #' to <- 'waco, texas'
 #' mapdist(from, to)
@@ -31,51 +30,53 @@
 #' mapdist(from, to)
 #'
 #' mapdist('the white house', 'washington monument', mode = 'walking')
-#' 
+#'
 #' # geographic coordinates are accepted as well
 #' (wh <- as.numeric(geocode('the white house')))
 #' (wm <- as.numeric(geocode('washington monument')))
 #' mapdist(wh, wm, mode = 'walking')
 #' mapdist('the white house', wm, mode = 'walking')
 #' distQueryCheck()
-#' 
-#' }
-#' 
-mapdist <- function(from, to, mode = c('driving','walking','bicycling'), 
-  output = c('simple','all'), messaging = FALSE, sensor = FALSE, 
+#'
+#'
+mapdist <- function(from, to, mode = c('driving','walking','bicycling'),
+  output = c('simple','all'), messaging = FALSE, sensor = FALSE,
   language = 'en-EN', override_limit = FALSE)
 {
-	
+
+  message("by using this function you are agreeing to the terms at :")
+  message("http://code.google.com/apis/maps/documentation/distancematrix/\n")
+
   # check parameters
   if(is.numeric(from) && length(from) == 2) from <- revgeocode(from)
   stopifnot(is.character(from))
   if(is.numeric(to) && length(to) == 2) to <- revgeocode(to)
-  stopifnot(is.character(to))  
+  stopifnot(is.character(to))
   from_to_df <- data.frame(from = from, to = to, stringsAsFactors = FALSE)
-  origins <- from_to_df$from 
+  origins <- from_to_df$from
   destinations <- from_to_df$to # this ensures # from = # to
-  mode <- match.arg(mode)    
-  output <- match.arg(output)  
+  mode <- match.arg(mode)
+  output <- match.arg(output)
   stopifnot(is.logical(messaging))
-  stopifnot(is.logical(sensor))  
-  
-  
+  stopifnot(is.logical(sensor))
+
+
   getdists <- function(df){
-  	
+
   	# format url
     origin <- df$from[1]
     origin <- gsub(',', '', origin)
     origin <- gsub(' ', '+', origin)
-    origin <- paste('origins=', origin, sep = '')      
+    origin <- paste('origins=', origin, sep = '')
     destinations <- df$to
     destinations <- gsub(',', '', destinations)
-    destinations <- gsub(' ', '+', destinations)  
-    destinations <- paste('destinations=', paste(destinations, collapse = '|'), sep = '')    
-    mode4url <- paste('mode=', mode, sep = '')   
-    lang4url <- paste('language=', language, sep = '')   
-    sensor4url <- paste('sensor=', tolower(as.character(sensor)), sep = '')   
-    posturl <- paste(origin, destinations, mode4url, sensor4url, sep = '&')    
-    url_string <- paste("http://maps.googleapis.com/maps/api/distancematrix/json?", 
+    destinations <- gsub(' ', '+', destinations)
+    destinations <- paste('destinations=', paste(destinations, collapse = '|'), sep = '')
+    mode4url <- paste('mode=', mode, sep = '')
+    lang4url <- paste('language=', language, sep = '')
+    sensor4url <- paste('sensor=', tolower(as.character(sensor)), sep = '')
+    posturl <- paste(origin, destinations, mode4url, sensor4url, sep = '&')
+    url_string <- paste("http://maps.googleapis.com/maps/api/distancematrix/json?",
       posturl, sep = "")
     url_string <- URLencode(url_string)
 
@@ -87,40 +88,46 @@ mapdist <- function(from, to, mode = c('driving','walking','bicycling'),
       return(
         rbind(
           getdists(df[half_df,]),
-          getdists(df[(half_df+1):n,])          
+          getdists(df[(half_df+1):n,])
         )
       )
     }
-    
-    # check/update google query limit
-    check_dist_query_limit(url_string, elems = nrow(df), 
-      override = override_limit, messaging = messaging)    
 
-  
+    # check/update google query limit
+    check_dist_query_limit(url_string, elems = nrow(df),
+      override = override_limit, messaging = messaging)
+
+
     # distance lookup
     if(messaging) message('trying url ', url_string)
-    connect <- url(url_string)    
+    connect <- url(url_string)
     tree <- fromJSON(paste(readLines(connect), collapse = ''))
     close(connect)
-    
+
     # message user
     message(paste0('Information from URL : ', url_string))
-    message('Google Maps API Terms of Service : http://developers.google.com/maps/terms')       
-    
-    # label destinations
-    names( tree$rows[[1]][[1]] ) <- df$to
-    
+
+    # label destinations - first check if all were found
+    if(length(df$to) != length(tree$destination_addresses)){
+      message("matching was not perfect, returning what was found.")
+      names( tree$rows[[c(1,1)]] ) <- tree$destination_addresses
+      output <<- "all"
+      # stringdist::amatch(df$to, tree$destination_addresses, maxDist = 10)
+    } else {
+      names( tree$rows[[c(1,1)]] ) <- df$to
+    }
+
     # return
-    tree$rows[[1]][[1]]
+    tree$rows[[c(1,1)]]
   }
-  
-  out <- dlply(from_to_df, .(from), getdists)
-  
+
+  out <- dlply(from_to_df, "from", getdists)
+
   # return all
   if(output == 'all') return(out)
-  
-  
-  
+
+
+
   # format output
   out <-
     ldply(out, function(oneFromList){
@@ -131,56 +138,57 @@ mapdist <- function(from, to, mode = c('driving','walking','bicycling'),
           miles = 0.0006214 * oneToList$distance$value,
           seconds = oneToList$duration$value,
           minutes = oneToList$duration$value / 60,
-  	      hours = oneToList$duration$value / 3600	  
-        )    
+  	      hours = oneToList$duration$value / 3600
+        )
       })
     })
+
   names(out) <- c('from', 'to', names(out)[3:ncol(out)])
-  
+
   # 'simple' return
-  suppressMessages(join(from_to_df, out))    
+  suppressMessages(join(from_to_df, out))
 }
 
 
 
 
-check_dist_query_limit <- function(url_string, elems, override, messaging){  
+check_dist_query_limit <- function(url_string, elems, override, messaging){
   .GoogleDistQueryCount <- NULL; rm(.GoogleDistQueryCount); # R CMD check trick
-  	
+
   if(exists('.GoogleDistQueryCount', .GlobalEnv)){
-    	
-    .GoogleDistQueryCount <<- 
+
+    .GoogleDistQueryCount <<-
       subset(.GoogleDistQueryCount, time >= Sys.time() - 24*60*60)
-    
+
     # 2500 per 24 hours
     if(sum(.GoogleDistQueryCount$elements) + elems > 2500){
-      message('query max exceeded, see ?mapdist.  current total = ', 
+      message('query max exceeded, see ?mapdist.  current total = ',
         sum(.GoogleDistQueryCount$elements))
       if(!override) stop('google query limit exceeded.', call. = FALSE)
     }
-    
+
     # 100 per 10 seconds
-    if(with(.GoogleDistQueryCount, 
+    if(with(.GoogleDistQueryCount,
       sum(elements[time >= Sys.time() - 10]) + elems > 100
     )){
       if(messaging) message('waiting 10 seconds for another 100 queries...', appendLF=F)
       Sys.sleep(10) # can do better
-      if(messaging) message(' done')      
-    }    
-      
+      if(messaging) message(' done')
+    }
+
     # append to .GoogleDistQueryCount
-    .GoogleDistQueryCount <<- rbind(.GoogleDistQueryCount, 
-      data.frame(time = Sys.time(),  url = url_string, 
+    .GoogleDistQueryCount <<- rbind(.GoogleDistQueryCount,
+      data.frame(time = Sys.time(),  url = url_string,
         elements = elems, stringsAsFactors = FALSE)
     )
-    
-    	
+
+
   } else {
-    	
-    .GoogleDistQueryCount <<- 
-      data.frame(time = Sys.time(),  url = url_string, 
+
+    .GoogleDistQueryCount <<-
+      data.frame(time = Sys.time(),  url = url_string,
         elements = elems, stringsAsFactors = FALSE)
-      
+
   }
 }
 
@@ -189,7 +197,7 @@ check_dist_query_limit <- function(url_string, elems, override, messaging){
 #' Check Google Maps Distance Matrix API query limit
 #'
 #' Check Google Maps Distance Matrix API query limit
-#' 
+#'
 #' @return a data frame
 #' @author David Kahle \email{david.kahle@@gmail.com}
 #' @seealso \url{http://code.google.com/apis/maps/documentation/distancematrix/}
@@ -197,8 +205,8 @@ check_dist_query_limit <- function(url_string, elems, override, messaging){
 #' @examples
 #' distQueryCheck()
 distQueryCheck <- function(){
-  .GoogleDistQueryCount <- NULL; rm(.GoogleDistQueryCount); # R CMD check trick	
-  if(exists('.GoogleDistQueryCount', .GlobalEnv)){    	
+  .GoogleDistQueryCount <- NULL; rm(.GoogleDistQueryCount); # R CMD check trick
+  if(exists('.GoogleDistQueryCount', .GlobalEnv)){
   	remaining <- 2500-sum(
   	  subset(.GoogleDistQueryCount, time >= Sys.time() - 24*60*60)$elements
   	  )
@@ -206,6 +214,6 @@ distQueryCheck <- function(){
   } else {
   	remaining <- 2500
     message(remaining, ' distance queries remaining.')
-  }	
+  }
   invisible(remaining)
 }
--- R/qmplot.R
+++ R/qmplot.R
@@ -1,26 +1,28 @@
 #' Quick map plot
 #'
-#' \code{qmplot} is the ggmap equivalent to the ggplot2 function qplot and allows for 
-#' the quick plotting of maps with data/models/etc.  qmplot is still experimental.
-#' 
-#' 
+#' \code{qmplot} is the ggmap equivalent to the ggplot2 function qplot and allows for
+#' the quick plotting of maps with data/models/etc.
+#'
+#'
 #' @param x longitude values
 #' @param y latitude values
 #' @param ... other aesthetics passed for each layer
-#' @param data data frame to use (optional).  If not specified, will create 
+#' @param data data frame to use (optional).  If not specified, will create
 #'   one, extracting vectors from the current environment.
 #' @param zoom map zoom, see \code{\link{get_map}}
 #' @param source map source, see \code{\link{get_map}}
-#' @param extent how much of the plot should the map take up? 'normal', 'panel', or 'device' (default)
-#' @param legend 'left', 'right' (default), 'bottom', 'top', 'bottomleft', 'bottomright', 'topleft', 'topright', 'none' (used with extent = 'device') 
-#' @param padding distance from legend to corner of the plot  (used with extent = 'device') 
+#' @param maptype map type, see \code{\link{get_map}}
+#' @param extent how much of the plot should the map take up? "normal", "panel", or "device" (default)
+#' @param legend "left", "right" (default), "bottom", "top", "bottomleft", "bottomright", "topleft", "topright", "none" (used with extent = "device")
+#' @param padding distance from legend to corner of the plot  (used with extent = "device")
+#' @param force force new map (don't use archived version)
 #' @param darken vector of the form c(number, color), where number is in [0, 1] and color is a character string indicating the color of the darken.  0 indicates no darkening, 1 indicates a black-out.
-#' @param mapcolor color ('color') or black-and-white ('bw')
+#' @param mapcolor color ("color") or black-and-white ("bw")
 #' @param facets faceting formula to use.  Picks \code{\link{facet_wrap}} or
 #'   \code{\link{facet_grid}} depending on whether the formula is one sided
 #'   or two-sided
 #' @param margins whether or not margins will be displayed
-#' @param geom character vector specifying geom to use.  defaults to 
+#' @param geom character vector specifying geom to use.  defaults to
 #'  "point"
 #' @param stat character vector specifying statistics to use
 #' @param position character vector giving position adjustment to use
@@ -32,24 +34,26 @@
 #' @param ylab character vector or expression for y axis label
 #' @export
 #' @examples
-#' \donttest{
+#' \dontrun{
+#' # these are skipped to conserve R check time
+#'
 #' qmplot(lon, lat, data = crime)
-#' 
-#' 
+#'
+#'
 #' # only violent crimes
 #' violent_crimes <- subset(crime,
-#'   offense != 'auto theft' & 
-#'   offense != 'theft' & 
-#'   offense != 'burglary'
+#'   offense != "auto theft" &
+#'   offense != "theft" &
+#'   offense != "burglary"
 #' )
-#' 
+#'
 #' # rank violent crimes
-#' violent_crimes$offense <- 
+#' violent_crimes$offense <-
 #'   factor(violent_crimes$offense,
-#'     levels = c('robbery', 'aggravated assault', 
-#'       'rape', 'murder')
+#'     levels = c("robbery", "aggravated assault",
+#'       "rape", "murder")
 #'   )
-#' 
+#'
 #' # restrict to downtown
 #' violent_crimes <- subset(violent_crimes,
 #'   -95.39681 <= lon & lon <= -95.34188 &
@@ -57,75 +61,192 @@
 #' )
 #'
 #' theme_set(theme_bw())
-#' 
-#' qmplot(lon, lat, data = violent_crimes, colour = offense, darken = .5, 
-#'   size = I(3.5), alpha = I(.6), legend = 'topleft')
-#' 
-#' qmplot(lon, lat, data = violent_crimes, geom = c('point','density2d'))
+#'
+#' qmplot(lon, lat, data = violent_crimes, colour = offense, darken = .5,
+#'   size = I(3.5), alpha = I(.6), legend = "topleft")
+#'
+#' qmplot(lon, lat, data = violent_crimes, geom = c("point","density2d"))
 #' qmplot(lon, lat, data = violent_crimes) + facet_wrap(~ offense)
-#' qmplot(lon, lat, data = violent_crimes, extent = 'panel') + facet_wrap(~ offense)
-#' qmplot(lon, lat, data = violent_crimes, extent = 'panel', colour = offense) + 
+#' qmplot(lon, lat, data = violent_crimes, extent = "panel") + facet_wrap(~ offense)
+#' qmplot(lon, lat, data = violent_crimes, extent = "panel", colour = offense,
+#'     darken = .4) +
 #'   facet_wrap(~ month)
 #'
-#' 
-#' 
-#' 
-#' # doesn't quite work yet....
-#' qmplot(long, lat, xend = long + delta_long, 
-#'   yend = lat + delta_lat, data = seals, geom = 'segment')
-#' 
+#'
+#'
+#'
+#' # doesn't quite work yet.... http://tile.stamen.com/terrain/#4/30.28/-87.21
+#' qmplot(long, lat, xend = long + delta_long,
+#'   yend = lat + delta_lat, data = seals, geom = "segment")
+#'
+#' # this works though!
+#' qmplot(long, lat, xend = long + delta_long, maptype = "toner-lite",
+#'   color = I("red"), yend = lat + delta_lat, data = seals,
+#'   geom = "segment", zoom = 5)
+#'
+#' qmplot(long, lat, xend = long + delta_long, maptype = "watercolor",
+#'   yend = lat + delta_lat, data = seals,
+#'   geom = "segment", zoom = 6)
+#'
 #'
 #' library(scales)
 #' library(grid)
-#' options('device')$device(width = 4.98, height = 5.97)
 #' qmplot(lon, lat, data = wind, size = I(.5), alpha = I(.5)) +
-#'   ggtitle('NOAA Wind Report Sites')   
+#'   ggtitle("NOAA Wind Report Sites")
 #'
 #' # thin down data set...
 #' s <- seq(1, 227, 8)
-#' thinwind <- subset(wind, 
-#'   lon %in% unique(wind$lon)[s] & 
+#' thinwind <- subset(wind,
+#'   lon %in% unique(wind$lon)[s] &
 #'   lat %in% unique(wind$lat)[s]
 #' )
-#' 
+#'
 #' # for some reason adding arrows to the following plot bugs
 #' theme_set(theme_bw(18))
-#' options('device')$device(width = 6.13, height = 7.04)
-#' qmplot(lon, lat, data = thinwind, geom = 'tile', fill = spd, alpha = spd,
-#'     legend = 'bottomleft') +
-#'   geom_leg(aes(xend = lon + delta_lon, yend = lat + delta_lat)) +
-#'   scale_fill_gradient2('Wind Speed\nand\nDirection', 
-#'     low = 'green', mid = muted('green'), high = 'red') +
-#'   scale_alpha('Wind Speed\nand\nDirection', range = c(.1, .75)) +
-#'   guides(fill = guide_legend(), alpha = guide_legend())
-#' 
-#' 
-#' 
-#' 
-#' }
 #'
-qmplot <- function(x, y, ..., data, zoom, source = 'stamen', 
-  extent = 'device', legend = 'right', padding = .02, darken = c(0, 'black'), 
-  mapcolor = 'color',
-  facets = NULL, margins = FALSE, geom = "auto", stat = list(NULL), 
-  position = list(NULL), xlim = c(NA, NA), ylim = c(NA, NA), main = NULL, f = 0.05, 
+#' qmplot(lon, lat, data = thinwind, geom = "tile", fill = spd, alpha = spd,
+#'     legend = "bottomleft") +
+#'   geom_leg(aes(xend = lon + delta_lon, yend = lat + delta_lat)) +
+#'   scale_fill_gradient2("Wind Speed\nand\nDirection",
+#'     low = "green", mid = muted("green"), high = "red") +
+#'   scale_alpha("Wind Speed\nand\nDirection", range = c(.1, .75)) +
+#'   guides(fill = guide_legend(), alpha = guide_legend())
+#'
+#'
+#'
+#'
+#' ## kriging
+#' ############################################################
+#' # the below examples show kriging based on undeclared packages
+#' # to better comply with CRAN's standards, we remove it from
+#' # executing, but leave the code as a kind of case-study
+#' # they also require the rgdal library
+#'
+#' if(FALSE){
+#'
+#' library(lattice)
+#' library(sp)
+#' library(rgdal)
+#'
+#' # load in and format the meuse dataset (see bivand, pebesma, and gomez-rubio)
+#' data(meuse)
+#' coordinates(meuse) <- c("x", "y")
+#' coordinates(meuse) <- ~x+y
+#' proj4string(meuse) <- CRS("+init=epsg:28992")
+#' meuse <- spTransform(meuse, CRS("+proj=longlat +datum=WGS84"))
+#'
+#' # plot
+#' plot(meuse)
+#'
+#' df <- as.data.frame(slot(meuse, "coords"))
+#' df <- data.frame(df, slot(meuse, "data"))
+#'
+#' qmplot(x, y, size = cadmium, alpha = I(.75), color = I("green"),
+#'   data = df, zoom = 14, legend = "topleft",
+#'   source = "google", maptype = "satellite", darken = .2
+#' ) + scale_size("Cadmium\n(ppm)")
+#'
+#'
+#' # load in the meuse.grid dataset (looking toward kriging)
+#' library(gstat)
+#' data(meuse.grid)
+#' coordinates(meuse.grid) <- c("x", "y")
+#' proj4string(meuse.grid) <- CRS("+init=epsg:28992")
+#' meuse.grid <- spTransform(meuse.grid, CRS("+proj=longlat +datum=WGS84"))
+#' mg <- as.data.frame(slot(meuse.grid, "coords"))
+#' mg <- data.frame(mg, slot(meuse.grid, "data"))
+#'
+#' # plot it
+#' qmplot(x, y, data = mg, source = "google")
+#'
+#'
+#' # using linear regression - surface trend analysis
+#' tsa <- krige(log(zinc) ~ 1, meuse, meuse.grid)
+#' mg$pred <- slot(tsa, "data")$var1.pred
+#' qmplot(x, y, data = mg, source = "google", color = pred,
+#'     size = I(2.5), shape = I(15),
+#'     alpha = I(3/5), darken = .3, legend = "topleft") +
+#'   scale_color_gradient("Predicted\nLog Zinc",
+#'     low = "green", high = "red"
+#'   )
+#'
+#' tsa <- krige(log(zinc) ~ 1, meuse, meuse.grid, degree = 2)
+#' mg$pred <- slot(tsa, "data")$var1.pred
+#' qmplot(x, y, data = mg, source = "google", color = pred,
+#'     size = I(2.5), shape = I(15),
+#'     alpha = I(3/5), darken = .3, legend = "topleft") +
+#'   scale_color_gradient("Predicted\nLog Zinc",
+#'     low = "green", high = "red"
+#'   )
+#'
+#'
+#' # ordinary kriging
+#' vgram <- variogram(log(zinc) ~ sqrt(dist), meuse)
+#' vgramFit <- fit.variogram(vgram, vgm(1, "Exp", 300, 1))
+#' ordinaryKrige <- krige(log(zinc) ~ 1, meuse, meuse.grid, vgramFit)
+#' mg$pred <- slot(ordinaryKrige, "data")$var1.pred
+#' qmplot(x, y, data = mg, source = "google", color = pred,
+#'     size = I(2.5), shape = I(15),
+#'     alpha = I(3/5), darken = .3, legend = "topleft") +
+#'   scale_color_gradient("Predicted\nLog Zinc",
+#'     low = "green", high = "red"
+#'   )
+#'
+#' # universal kriging
+#' universalKrige <- krige(log(zinc) ~ sqrt(dist), meuse, meuse.grid, vgramFit)
+#' mg$pred <- slot(universalKrige, "data")$var1.pred
+#' qmplot(x, y, data = mg, source = "google", color = pred,
+#'     size = I(2.5), shape = I(15),
+#'     alpha = I(3/4), darken = .3, legend = "topleft") +
+#'   scale_color_gradient("Predicted\nLog Zinc",
+#'     low = "green", high = "red"
+#'   )
+#'
+#'
+#'
+#' # kriging with data
+#' qmplot(x, y, data = mg, source = "google", color = pred,
+#'     size = I(2.5), shape = I(15),
+#'     alpha = I(3/4), darken = .3, legend = "topleft") +
+#'   scale_color_gradient("Predicted\nLog Zinc",
+#'     low = "green", high = "red"
+#'   ) +
+#'   geom_point(
+#'     aes(x = x, y = y, size = log(zinc)), data = df,
+#'     color = "black", alpha = 1/2
+#'   ) +
+#'   scale_size("Observed\nLog Zinc")
+#'
+#'
+#'
+#' } # end FALSE if
+#'
+#'
+#'
+#' } # end dontrun
+#'
+qmplot <- function(x, y, ..., data, zoom, source = "stamen", maptype = "terrain",
+  extent = "device", legend = "right", padding = .02, force = FALSE,
+  darken = c(0, "black"), mapcolor = "color",
+  facets = NULL, margins = FALSE, geom = "auto", stat = list(NULL),
+  position = list(NULL), xlim = c(NA, NA), ylim = c(NA, NA), main = NULL, f = 0.05,
   xlab = deparse(substitute(x)), ylab = deparse(substitute(y)))
 {
-	
+
   argnames <- names(as.list(match.call(expand.dots=FALSE)[-1]))
   arguments <- as.list(match.call()[-1])
-  
-  args <- as.list(match.call(expand.dots = TRUE)[-1])  
+
+  args <- as.list(match.call(expand.dots = TRUE)[-1])
   argsgiven <- names(args)
-  if('mapcolour' %in% argsgiven) mapcolor <- eval(args$mapcolour)
-  
-  .all_aesthetics <- unlist(getAnywhere(.all_aesthetics)[1:42])  
+  if("mapcolour" %in% argsgiven) mapcolor <- eval(args$mapcolour)
+
+  .all_aesthetics <- unlist(getAnywhere(.all_aesthetics)[1:42])
   aesthetics <- compact(arguments[.all_aesthetics])
-  aesthetics <- aesthetics[!ggplot2:::is.constant(aesthetics)]
+  aesthetics <- aesthetics[!is.constant(aesthetics)]
   aes_names <- names(aesthetics)
-  aesthetics <- ggplot2:::rename_aes(aesthetics)
+  aesthetics <- rename_aes(aesthetics)
   class(aesthetics) <- "uneval"
-  
+
   if (missing(data)) {
     # If data not explicitly specified, will be pulled from workspace
     data <- data.frame()
@@ -144,7 +265,7 @@ qmplot <- function(x, y, ..., data, zoom, source = 'stamen',
       geom[geom == "auto"] <- "point"
       stat <- "qq"
     } else if (missing(y)) {
-      stop('y must be provided for quickmap.', call. = FALSE)
+      stop("y must be provided for quickmap.", call. = FALSE)
     } else {
       if (missing(x)) {
         aesthetics$x <- bquote(seq_along(.(y)), aesthetics)
@@ -154,84 +275,97 @@ qmplot <- function(x, y, ..., data, zoom, source = 'stamen',
   }
 
   env <- parent.frame()
-  
-  # calculate map dimensions    
+
+  # calculate map dimensions
   bbox <- make_bbox(
-    lon = data[,deparse(substitute(x))], 
+    lon = data[,deparse(substitute(x))],
     lat = data[,deparse(substitute(y))],
     f = f
-  )    
-  lon_range <- bbox[c('left','right')]
-  lat_range <- bbox[c('bottom','top')]  
-  
+  )
+  lon_range <- bbox[c("left","right")]
+  lat_range <- bbox[c("bottom","top")]
+
+
   # compute zoom
   if(missing(zoom)){
-    lonlength <- diff(lon_range)
-    latlength <- diff(lat_range)    
-    zoomlon <- ceiling( log2( 360*2 / lonlength) )
-    zoomlat <- ceiling( log2( 180*2 / latlength) )    
-    zoom <- max(zoomlon, zoomlat)
+    zoom <- calc_zoom(lon_range, lat_range)
+    message(paste0("Using zoom = ", zoom, "..."))
   }
-  
+
+
   # get map
-  map <- get_map(location = bbox, zoom = zoom, source = source, color = mapcolor)
+  if(source == "google"){
+    map <- get_map(location = c(mean(lon_range), mean(lat_range)), zoom = zoom,
+      source = source, color = mapcolor,
+      maptype = maptype, force = force
+    )
+  } else { # bounding box
+    map <- get_map(location = bbox, zoom = zoom,
+      source = source, color = mapcolor,
+      maptype = maptype, force = force
+    )
+  }
   xmin <- attr(map, "bb")$ll.lon
   xmax <- attr(map, "bb")$ur.lon
   ymin <- attr(map, "bb")$ll.lat
-  ymax <- attr(map, "bb")$ur.lat  
-  
+  ymax <- attr(map, "bb")$ur.lat
+
+
   # check darken
   stopifnot(0 <= as.numeric(darken[1]) && as.numeric(darken[1]) <= 1)
-  if(length(darken) == 1 & is.numeric(darken)) darken <- c(darken, 'black')
-  
+  if(length(darken) == 1 & is.numeric(darken)) darken <- c(darken, "black")
+
+
   # initialize plot
   p <- ggplot(data, aesthetics, environment = env) +
-    inset_raster(map, xmin, xmax, ymin, ymax) + 
-    annotate('rect', xmin=xmin, xmax=xmax, ymin=ymin, ymax=ymax, 
+    inset_raster(map, xmin, xmax, ymin, ymax) +
+    annotate("rect", xmin=xmin, xmax=xmax, ymin=ymin, ymax=ymax,
   	  fill = darken[2], alpha = as.numeric(darken[1])) +
     coord_map(projection = "mercator")
-    
+
+
   # enforce extent
-  if(extent == 'normal'){
+  if(extent == "normal"){
     # nothing
-  } else if(extent == 'panel'){
+  } else if(extent == "panel"){
   	p <- p +
       scale_x_continuous(lim = c(xmin, xmax), expand = c(0,0)) +
-      scale_y_continuous(lim = c(ymin, ymax), expand = c(0,0))           
-  } else if(extent == 'device'){
+      scale_y_continuous(lim = c(ymin, ymax), expand = c(0,0))
+  } else if(extent == "device"){
   	p <- p +
       scale_x_continuous(lim = c(xmin, xmax), expand = c(0,0)) +
       scale_y_continuous(lim = c(ymin, ymax), expand = c(0,0)) +
-      theme_nothing(legend = TRUE)    
-      
+      theme_nothing(legend = TRUE)
+
+
     # legend for full device map
-    if(legend %in% c('topleft','topright','bottomleft','bottomright')){
-      if(legend == 'bottomleft'){
+    if(legend %in% c("topleft","topright","bottomleft","bottomright")){
+      if(legend == "bottomleft"){
         lp <- c(padding, padding)
         lj <- c(0,0)
-      } else if(legend == 'topleft'){
+      } else if(legend == "topleft"){
         lp <- c(padding, 1-padding)
         lj <- c(0,1)
-      } else if(legend == 'bottomright'){
+      } else if(legend == "bottomright"){
         lp <- c(1-padding, padding)
         lj <- c(1,0)
-      } else if(legend == 'topright'){
+      } else if(legend == "topright"){
         lp <- c(1-padding, 1-padding)
         lj <- c(1,1)
       }
       p <- p + theme(
         legend.position = lp, legend.justification = lj,
         legend.background = element_rect(
-          fill = 'white', colour = 'gray80', size = .2
-        )        
+          fill = "white", colour = "gray80", size = .2
+        )
       )
-    } else if(legend %in% c('left','right','bottom','top')){
+    } else if(legend %in% c("left","right","bottom","top")){
       p <- p + theme(legend.position = legend)
-    } # else legend = 'none' as part of theme_nothing()
+    } # else legend = "none" as part of theme_nothing()
   }
-      
-    
-  
+
+
+
   if (is.null(facets)) {
     p <- p + facet_null()
   } else if (is.formula(facets) && length(facets) == 2) {
@@ -239,30 +373,31 @@ qmplot <- function(x, y, ..., data, zoom, source = 'stamen',
   } else {
     p <- p + facet_grid(facets = deparse(facets), margins = margins)
   }
-  
+
   if (!is.null(main)) p <- p + theme("title" = main)
 
   # Add geoms/statistics
   if (is.proto(position)) position <- list(position)
-  
+
   mapply(function(g, s, ps) {
-    if(is.character(g)) g <- ggplot2:::Geom$find(g)
-    if(is.character(s)) s <- ggplot2:::Stat$find(s)
-    if(is.character(ps)) ps <- ggplot2:::Position$find(ps)
+
+    if(is.character(g)) g <- Geom$find(g)
+    if(is.character(s)) s <- Stat$find(s)
+    if(is.character(ps)) ps <- Position$find(ps)
 
     params <- arguments[setdiff(names(arguments), c(aes_names, argnames))]
     params <- lapply(params, eval, parent.frame(n=1))
-    
+
     p <<- p + layer(geom=g, stat=s, geom_params=params, stat_params=params, position=ps)
   }, geom, stat, position)
-  
+
   if (!missing(xlab)) p <- p + xlab(xlab)
   if (!missing(ylab)) p <- p + ylab(ylab)
-  
+
   if (!missing(xlim)) p <- p + xlim(xlim)
   if (!missing(ylim)) p <- p + ylim(ylim)
-  
 
-  
+
+
   p
 }
